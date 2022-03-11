9c61cc8375b022ddccfafadd42c5c1e39ebdcec1
HabibIbrahimov <habibyi@code.edu.az>
2022-03-11 14:55:15 +0400

    feat:Category Controller

---
+++ BookSaleWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BookSaleWeb.Data;
+using BookSaleWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSaleWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
--- BookSaleWeb/Program.cs
+++ BookSaleWeb/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
