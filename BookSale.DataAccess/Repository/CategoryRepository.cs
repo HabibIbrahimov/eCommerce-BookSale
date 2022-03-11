48f8b6a23ec4de04169b1a8b5e9d9b2e70fde8d5
HabibIbrahimov <habibyi@code.edu.az>
2022-03-11 19:38:15 +0400

    feat:Category Repository Interface

---
+++ BookSale.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using BookSale.DataAccess.Repository.IRepository;
+using BookSale.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSale.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
