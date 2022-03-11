fd0c38c60712a1639851d8acc780da8f784d9fdc
HabibIbrahimov <habibyi@code.edu.az>
2022-03-11 19:27:53 +0400

    feat:IRepository Interface

--- BookSaleWeb/Controllers/CategoryController.cs
+++ BookSaleWeb/Controllers/CategoryController.cs
@@ -49,16 +49,16 @@ namespace BookSaleWeb.Controllers
             {
                 return NotFound();
             }
-            var categoryFromDb = _db.Categories.Find(id);
-            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //var categoryFromDb = _db.Categories.Find(id);
+            var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Name=="id");
             //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
 
-            if (categoryFromDb == null)
+            if (categoryFromDbFirst == null)
             {
                 return NotFound();
             }
 
-            return View(categoryFromDb);
+            return View(categoryFromDbFirst);
         }
 
         //POST
