ad11dda1eafa67e1fa81aa4f6f300e4b6448d05f
HabibIbrahimov <habibyi@code.edu.az>
2022-03-11 14:39:50 +0400

    Created porject and category model

---
+++ BookSaleWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookSaleWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookSaleWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
