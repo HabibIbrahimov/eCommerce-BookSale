c2a6b779ef803373afe2de29c5386516df08ce37
HabibIbrahimov <habibyi@code.edu.az>
2022-03-12 17:23:22 +0400

    feat:Product Repository

---
+++ BookSale.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookSale.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSale.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+        
+    }
+}
