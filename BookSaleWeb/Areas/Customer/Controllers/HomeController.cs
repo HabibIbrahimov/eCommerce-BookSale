d9b7f257f81cf90f293133e60f99bcd745b2660e
HabibIbrahimov <habibyi@code.edu.az>
2022-03-13 13:46:46 +0400

    feat:Products View Component

--- BookSaleWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookSaleWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using BookSale.Models;
+﻿using BookSale.DataAccess.Repository.IRepository;
+using BookSale.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,10 +9,12 @@ namespace BookSaleWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
---
+++ BookSaleWeb/ViewComponents/ProductsViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using BookSale.DataAccess;
+using BookSale.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookSaleWeb.ViewComponents
+{
+    public class ProductsViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductsViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Product> products = _db.Products.Include(p => p.Category).Take(6).ToList();
+            return View (await Task.FromResult(products));
+        }
+    }
+}
