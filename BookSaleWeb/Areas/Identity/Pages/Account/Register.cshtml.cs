05230c8a073fd56e71e763fd0c5819340c36f379
HabibIbrahimov <habibyi@code.edu.az>
2022-03-24 16:49:54 +0400

    feat: Hide Roles from Registration

--- BookSaleWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookSaleWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -204,8 +204,17 @@ namespace BookSaleWeb.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New User Created Successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                           
+                        }
                         return LocalRedirect(returnUrl);
+
                     }
                 }
                 foreach (var error in result.Errors)
