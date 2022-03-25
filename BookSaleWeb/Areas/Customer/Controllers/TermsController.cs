39b680304af354a6ad0bd5f7fb0a1a9d45ac35c8
HabibIbrahimov <habibyi@code.edu.az>
2022-03-25 18:31:09 +0400

    feat:Terms Page Created

---
+++ BookSaleWeb/Areas/Customer/Controllers/TermsController.cs
@@ -0,0 +1,23 @@
+﻿using BookSale.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSaleWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class TermsController : Controller
+    {
+        private readonly ILogger<TermsController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TermsController(ILogger<TermsController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
