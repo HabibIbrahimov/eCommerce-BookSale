6fb8afaa8f2338435aad1b0380b196af859f2c28
HabibIbrahimov <habibyi@code.edu.az>
2022-03-25 18:19:46 +0400

    feat:Testimonials Page Created

---
+++ BookSaleWeb/Areas/Customer/Controllers/TestimonialsController.cs
@@ -0,0 +1,23 @@
+﻿using BookSale.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSaleWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class TestimonialsController : Controller
+    {
+        private readonly ILogger<TestimonialsController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TestimonialsController(ILogger<TestimonialsController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
