6c527c69be931ad33f1ab448727cf2ed267bef8b
HabibIbrahimov <habibyi@code.edu.az>
2022-03-28 16:33:45 +0400

    feat:About Us Page

---
+++ BookSaleWeb/Areas/Customer/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using BookSale.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSaleWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class AboutController : Controller
+    {
+        private readonly ILogger<AboutController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AboutController(ILogger<AboutController> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
